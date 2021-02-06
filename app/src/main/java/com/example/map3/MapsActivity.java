3145532b6f8ffd669624eca24ac32f2eb5f31c6f
Mngatia <nyawirahngatia@gmail.com>
2021-02-06 20:20:31 +0300

    Draw circles and Stroke pattern on/in Google Maps.

--- app/src/main/java/com/example/map3/MapsActivity.java
+++ app/src/main/java/com/example/map3/MapsActivity.java
@@ -12,11 +12,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.Dash;
+import com.google.android.gms.maps.model.Dot;
+import com.google.android.gms.maps.model.Gap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PatternItem;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
@@ -39,10 +47,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng m1 = new LatLng(20 , 40);
-//        LatLng m2 = new LatLng(25 , 45);
-//        LatLng m3 = new LatLng(30 , 50);
-//        LatLng m4 = new LatLng(45 , 55);
-//        LatLng m5 = new LatLng(60 , 90);
+        LatLng m2 = new LatLng(25 , 45);
+        LatLng m3 = new LatLng(30 , 50);
+        LatLng m4 = new LatLng(45 , 55);
+        LatLng m5 = new LatLng(60 , 90);
 
 
         marker = mMap.addMarker(new MarkerOptions()
@@ -52,14 +60,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .snippet("Lat")
         );
 
-        //Circle
-        CircleOptions circlen = new CircleOptions()
-                .center(m1)
-                .radius(10000); //in meters
+        //List
+        List<PatternItem> pattern = Arrays.asList(
+                new Dot() ,
+                new Gap(10),
+                new Dash(50)
 
-        //Get back the mutable circle
-        Circle circle = mMap.addCircle(circlen);
-        circle.setStrokeColor(Color.BLUE);
+        );
+
+        //Polyline
+        PolylineOptions opt = new PolylineOptions()
+                .add(m1)
+                .add(m2)
+                .add(m3)
+                .add(m4)
+                .add(m5);
+
+        //Polyline
+        Polyline p = mMap.addPolyline(opt); //by default
+
+        p.setPattern(pattern);
+
+
+        //Circle
+//        CircleOptions circlen = new CircleOptions()
+//                .center(m1)
+//                .radius(10000); //in meters
+//
+//        //Get back the mutable circle
+//        Circle circle = mMap.addCircle(circlen);
+//        circle.setStrokeColor(Color.BLUE);
 
 //        //Polyline
 //        PolylineOptions recOption = new PolylineOptions()
